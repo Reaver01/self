75b73449170b4dab87efb0f0542bcf749147fbeb
reaver01 <reaver_01@gmx.com>
2017-02-01 00:22:13 +0100

    make a nil check

--- lib/bot/module/event.rb
+++ lib/bot/module/event.rb
@@ -3,11 +3,15 @@ module Events
   extend Discordrb::EventContainer
   message do |event|
     if event.message.channel.pm?
-    elsif BOT.parse_mention(event.message.content).id == 150_278_590_494_277_632
-      if $settings.key?('afk')
-        if $settings['afk']
-          event.respond 'I am curretnly AFK. I will check my mentions when ' \
-                        'I return.'
+    else
+      unless BOT.parse_mention(event.message.content).nil?
+        if BOT.parse_mention(event.message.content).id == 150_278_590_494_277_632
+          if $settings.key?('afk')
+            if $settings['afk']
+              event.respond 'I am curretnly AFK. I will check my mentions when ' \
+                            'I return.'
+            end
+          end
         end
       end
     end
